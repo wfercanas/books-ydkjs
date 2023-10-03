27a2de79380cafb5d1cde75a6b578153786bdcf4
wfercanas <fernando.canas@outlook.com>
2023-10-03 15:54:06 -0500

    docs: tc39 groupBy function exercise

---
+++ getting_started/tc39/grouping.js
@@ -0,0 +1,37 @@
+/*
+  Given an array of elements and a characteristic, make groups depending on such characteristic
+  1- Separate odd numbers from even numbers
+  2- Separate vowels from consonants
+  3- Separate words by length
+*/
+
+// Should return {odd: [1, 3, 5], even: [2, 4]}
+const numbers = [1, 2, 3, 4, 5];
+
+// Should return {vowels: ['a', 'e'], consonants: ['f', 'k', 'j', 'm']}
+const letters = ["a", "f", "e", "k", "j", "m"];
+
+// Should return {'3': ["sun", "ant", "mic"], '4': ["hello", "flow", "mega", "init", "mark"], '5': ["fever", "looks", "finds", "reads"], '8': ["december"]}
+const words = [
+  "hello",
+  "sun",
+  "flow",
+  "ant",
+  "mic",
+  "fever",
+  "mega",
+  "looks",
+  "finds",
+  "reads",
+  "init",
+  "mark",
+  "december",
+];
+
+// Iteration -----------------------------------------
+
+// GroupBy -------------------------------------------
+
+// Test functions ------------------------------------
+
+// Executions ----------------------------------------
