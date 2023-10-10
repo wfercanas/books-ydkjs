2b0874bafac17278a489384d388ca2125da9191b
wfercanas <fernando.canas@outlook.com>
2023-10-10 15:44:06 -0500

    feat: split letters and numbers ex. in two files

--- getting_started/1_language/groupLetters.js
+++ getting_started/1_language/groupLetters.js
@@ -1,34 +1,13 @@
 /*
-  These exercises are related to the stage 3 proposal about creating Object.groupBy() and Map.groupBy() methods.
+  The exercise is related to the stage 3 proposal about creating Object.groupBy() and Map.groupBy() methods.
   
   Given an array of elements and a characteristic, make groups depending on such characteristic
   1- Separate odd numbers from even numbers
   2- Separate vowels from consonants
-  3- Separate words by length
 */
-
-const numbers = [1, 2, 3, 4, 5];
-// Should return {odd: [1, 3, 5], even: [2, 4]}
 
 const letters = ["a", "f", "e", "k", "j", "m"];
 // Should return {vowels: ['a', 'e'], consonants: ['f', 'k', 'j', 'm']}
-
-const words = [
-  "hello",
-  "sun",
-  "flow",
-  "ant",
-  "mic",
-  "fever",
-  "mega",
-  "looks",
-  "finds",
-  "reads",
-  "init",
-  "mark",
-  "december",
-];
-// Should return {'3': ["sun", "ant", "mic"], '4': ["hello", "flow", "mega", "init", "mark"], '5': ["fever", "looks", "finds", "reads"], '8': ["december"]}
 
 /* Traditional  -----------------------------------------
   Code here an implementation using a normal or traditional way of achieving the desired result
---
+++ getting_started/1_language/groupNumbers.js
@@ -0,0 +1,26 @@
+/*
+  The exercise is related to the stage 3 proposal about creating Object.groupBy() and Map.groupBy() methods.
+  
+  Given an array of elements and a characteristic, make groups depending on such characteristic
+  1- Separate odd numbers from even numbers
+  2- Separate vowels from consonants
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+// Should return {odd: [1, 3, 5], even: [2, 4]}
+
+/* Traditional  -----------------------------------------
+  Code here an implementation using a normal or traditional way of achieving the desired result
+*/
+
+/* GroupBy -------------------------------------------
+  Code here the implementation using Object.groupBy(). Make sure that your version of Node supports the method.
+*/
+
+/* Test functions ------------------------------------
+  Code here testing functions so you can visually check the expected vs. real results
+*/
+
+/* Executions ----------------------------------------
+  Execute here the testing functions
+*/
