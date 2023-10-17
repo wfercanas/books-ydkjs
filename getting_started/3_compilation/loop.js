e6ba9cfb708e83fee3149db480d868d57ea2c2aa
wfercanas <fernando.canas@outlook.com>
2023-10-17 15:50:24 -0500

    feat(compilation): exercises for checking ASTs

---
+++ getting_started/3_compilation/loop.js
@@ -0,0 +1,18 @@
+/*
+  Factorial using loops
+
+  1- Given a number, calculate the factorial of such number and print it in console.
+  2- Copy your code and see its AST
+
+  Examples: 
+  --> 5! = 5*4*3*2*1
+  --> 2! = 2*1  
+  --> 0! = 1
+  --> 1! = 1
+*/
+
+const number = 5;
+
+// Implementation:
+
+// Test:
