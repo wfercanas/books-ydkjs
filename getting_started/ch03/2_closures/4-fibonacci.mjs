4ea8a507103da8271cc2aa5f88ff90398d2bd6af
wfercanas <fernando.canas@outlook.com>
2024-01-25 15:47:43 -0500

    feat: change closure exercises to mjs

--- getting_started/ch03/2_closures/4-fibonacci.mjs
+++ getting_started/ch03/2_closures/4-fibonacci.mjs
@@ -4,3 +4,5 @@
   3. Such function should return a function called nextFibonacci
   4. Each time nextFibonacci is executed, it should print the next fibonacci number in the sequence
 */
+
+// 0, 1, 1, 2, 3, 5, 8,
