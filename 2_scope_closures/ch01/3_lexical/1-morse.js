60d06438fcff838eea976d539cd6a260b06226ef
wfercanas <fernando.canas@outlook.com>
2024-02-26 21:24:57 -0500

    feat: exercise for lexical scope

---
+++ 2_scope_closures/ch01/3_lexical/1-morse.js
@@ -0,0 +1,21 @@
+/*
+  Exercise:
+
+  1- The objective of this project is to create a telegraph
+  2- The user should be able to decide if he/she wants to translate strings into morse code or viceversa.
+  3- If the user input is a string, the program should print the translation into morse code and viceversa.
+  4- The morse code specifies silences between characters, replace such silences with spaces.
+  5- The morse code specifies silences between words, replace such silences with a slash.
+  6- The point can be replaced with a dot '.'
+  7- The dash can be replaced with a '-'.
+  8- The software has to implement testing. 
+  9- The software should be executed and the input of the user captured in runtime. Use readline() in nodejs for that.
+  
+  Example: 
+  -> 'hello' should print '.... . .-.. .-.. --- / .-- --- .-. .-.. -..'
+  -> More tests can be generated with https://morsecode.world/international/translator.html
+
+  Questions:
+  - How many buckets did you create?
+  - How many sources vs targets?
+*/
