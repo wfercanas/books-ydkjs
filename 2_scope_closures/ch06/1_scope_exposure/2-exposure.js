1f128d4d7485ae02e54dfab78ab0839ac1467710
wfercanas <fernando.canas@outlook.com>
2024-07-25 10:05:41 -0500

    feat: improve scope exposure exercise description

--- 2_scope_closures/ch06/1_scope_exposure/2-exposure.js
+++ 2_scope_closures/ch06/1_scope_exposure/2-exposure.js
@@ -1,7 +1,7 @@
 /*
   1- This challenge is about using what you learned about reducing scope exposure and memoization
-  2- We need you to provide a function called sumSquares, which sums the square values of integer values
-  3- For example: sumSquares(0, 2) = 0^2 + 1^2 + 2^2 = 5
-  4- sumSquares receives two arguments: start and end. Being both inclusive in the sum.
-  5- Use memoization to improve the performance of the function while controlling scope exposure
+  2- We need you to create two functions: sumAllSquares and sumRangeSquares.
+  3- sumAllSquares sums all the square values from 0 up to the integer provided as arguments: sumAllSquares(2) = 0^2 + 1^2 + 2^2 = 5
+  4- sumRangeSquares sums all the square values from start to end depending on the provided arguments: sumRangeSquares(1, 3) = 1^2 + 2^2 + 3^2 = 14
+  5- Use memoization in a single place to improve the performance of both functions while reducing scope exposure.
 */
