cafdd318d026974855b2d692ec86243c1c08eb70
wfercanas <fernando.canas@outlook.com>
2024-08-06 16:52:31 -0500

    feat: exercises for function declaration in blocks

---
+++ 2_scope_closures/ch06/3_function_declarations_in_blocks/1-ask.js
@@ -0,0 +1,15 @@
+// 2 -->
+// "use strict";
+
+if (false) {
+  // 1 ->
+  // function ask() {
+  //   console.log("Does this run?");
+  // }
+}
+
+ask();
+
+// What happens before uncommenting -->
+// What happens after uncommenting 1 -->
+// What happens after uncommenting 2 -->
---
+++ 2_scope_closures/ch06/3_function_declarations_in_blocks/2-ask.js
@@ -0,0 +1,35 @@
+// 4 -->
+// "use strict";
+
+if (true) {
+  function ask() {
+    console.log("Am I Called?");
+  }
+}
+
+// 1 -->
+// if (true) {
+//   function ask() {
+//     console.log("Or what about me?");
+//   }
+// }
+
+// 2 -->
+// for (let i = 0; i < 5; i++) {
+//   function ask() {
+//     console.log("Or is it one of these?");
+//   }
+// }
+
+// 3 -->
+// function ask() {
+//   console.log("Wait, maybe, it's this one?");
+// }
+
+ask();
+
+// What happens before uncommenting -->
+// What happens after uncommenting 1 -->
+// What happens after uncommenting 2 -->
+// What happens after uncommenting 3 -->
+// What happens after uncommenting 4 -->
